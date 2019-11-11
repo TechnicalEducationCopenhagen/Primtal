be18a638e9940f394bf2e58e165b46db95b3ee5e
Søren Magnusson <smag@tec.dk>
2019-11-11 10:12:29 +0100

    Add project files.

---
+++ Simpel/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Simpel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
